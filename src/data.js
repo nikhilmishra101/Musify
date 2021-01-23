57369aa4ea0d4d7db0b187e787bc4376869180bd
IEIrodov <nikhilindia30111999@gmail.com>
2021-01-23 10:36:37 +0530

    Music App Ready

---
+++ src/data.js
@@ -0,0 +1,108 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Lax Incense",
+      artist: "Mama Aiuto, Daphné",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/01/6b1bb8736ee3e972747bc11f312e31cf7f5823e4-1024x1024.jpg",
+      id: uuidv4(),
+      active: true,
+      color: ["#501718", "#0D2122"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12125",
+    },
+    {
+      name: "Plants",
+      artist: "Philanthrope, Ian Ewing, Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#865446", "#C3AB8D"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8036",
+    },
+    {
+      name: "After Dark",
+      artist: "Sugi.wa",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/be9e5dca9d4c9015c407a2d2590af8a0e5e278ce-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C5FDD6", "#D3806E"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8105",
+    },
+    {
+      name: "Today Feels Like Everyday",
+      artist: "Mama Aiuto",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/01/6b1bb8736ee3e972747bc11f312e31cf7f5823e4-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#501718", "#0D2122"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=12128",
+    },
+    {
+      name: "Calm",
+      artist: "ØDYSSEE, Florent Garcia",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/d9d8ae69141cb7838e3c53bbf393c6218285e384-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#8093FF", "#06004D"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9285",
+    },
+    {
+      name: "Backyard Puddles",
+      artist: "Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#91C9D8", "#1F1C36"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10032",
+    },
+    {
+      name: "Keep Going",
+      artist: "Swørn",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E29C97", "#601B5C"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+    },
+    {
+      name: "Cloud Carpets",
+      artist: "The Field Tapes, Ezzy, Wowflower",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#6C7676", "#3D5172"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11248",
+    },
+    {
+      name: "Oceania",
+      artist: "chromonicci",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FACEB5", "#C7582B"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7966",
+    },
+    {
+      name: "Ocean View",
+      artist: "G Mills, Kyle McEvoy, Luke Otwell",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E2C6D4", "#133362"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10320",
+    },
+  ];
+}
+
+export default chillHop;
